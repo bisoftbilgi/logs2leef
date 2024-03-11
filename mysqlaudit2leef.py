d5dbf6b6f4510ed4a79a64d45625a3a863f760c8
Ozgur Yuksel <ozgur.yuksel@bisoft.com.tr>
2024-03-11 18:01:59 +0300

    Read from input (audit log file) in an infinite loop and print LEEF lines out.

--- mysqlaudit2leef.py
+++ mysqlaudit2leef.py
@@ -2,8 +2,82 @@
 
 import xml.etree.ElementTree as ET
 
-audit_data = '''
-<AUDIT>
+
+def printLEEFentry(audit_record_data):
+    # Parse the XML stanza
+    audit_record = ET.fromstring(audit_record_data)
+
+    # Parse AUDIT_RECORD element
+    timestamp = audit_record.find('TIMESTAMP').text
+    record_id = audit_record.find('RECORD_ID').text
+    op_name = audit_record.find('NAME').text
+    try:
+        user_info = audit_record.find('USER').text
+    except AttributeError:
+        user_info = 'N/A'
+
+    try:
+        hostname = audit_record.find('HOST').text
+    except AttributeError:
+        hostname = 'N/A'
+
+    try:
+        IP_address = audit_record.find('IP').text
+    except AttributeError:
+        IP_address = 'N/A'
+
+    try:
+        command_class = audit_record.find('COMMAND_CLASS').text
+    except AttributeError:
+        command_class = 'N/A'
+
+    try:
+        sqltext = audit_record.find('SQLTEXT').text
+    except AttributeError:
+        sqltext = 'N/A'
+
+    # Print syslog header
+    # Need to convert timestamp into RFC 5424
+    # TODO replace/convert timezone appropriately (need sample data to decide how)
+    # examples:
+    #    2019-01-18T11:07:53.520+07:00 
+    #    2019-01-18T11:07:53.520Z
+    print(f"<13>1 {timestamp} {hostname}",end=' ')
+    
+    # Print LEEF 2.0 header
+    print(f"2.0|BiSoft|MySQL Auditing|1.0|{record_id}|^|",end=' ')
+
+    # Print Event attributes
+    print(f"usrName={user_info}^sev=1^src={IP_address}^auditName={op_name}^commClass={command_class}^sqlText={sqltext}")
+
+
+# Main mysqlaudit2leef.py 
+
+while True:
+    # TODO: Read from input until matching "<AUDIT_RECORD>"
+    inputline = ''
+    while "<AUDIT_RECORD>" not in inputline:
+        inputline = input()
+
+    #print(f"Inputline: {inputline}")
+    #print(">>>>>> New Audit Record")
+    # TODO: Start initializing AUDIT_RECORD multiline text variable
+    audit_record_text = inputline
+
+    # TODO: Until matching "</AUDIT_RECORD>"
+    while "</AUDIT_RECORD>" not in inputline:
+        inputline = input()
+        #print(f"Inputlineinloop: {inputline}")
+        # TODO: Append every new line 
+        audit_record_text = audit_record_text + inputline
+        #print(f"Auditrecordinloop: {audit_record_text}")
+
+    #print(audit_record_text)
+    printLEEFentry(audit_record_text)
+    #print(">>>>>> End of Audit Record")
+
+
+audit_record_text = '''
  <AUDIT_RECORD>
   <TIMESTAMP>2019-10-03T14:09:38 UTC</TIMESTAMP>
   <RECORD_ID>6_2019-10-03T14:06:33</RECORD_ID>
@@ -18,34 +92,5 @@ audit_data = '''
   <COMMAND_CLASS>drop_table</COMMAND_CLASS>
   <SQLTEXT>DROP TABLE IF EXISTS t</SQLTEXT>
  </AUDIT_RECORD>
-</AUDIT>
 '''
 
-# Parse the XML stanza
-audit = ET.fromstring(audit_data)
-
-# Iterate through each AUDIT_RECORD element
-for audit_record in audit.findall('AUDIT_RECORD'):
-    timestamp = audit_record.find('TIMESTAMP').text
-    record_id = audit_record.find('RECORD_ID').text
-    op_name = audit_record.find('NAME').text
-    user_info = audit_record.find('USER').text
-    hostname = audit_record.find('HOST').text
-    IP_address = audit_record.find('IP').text
-    command_class = audit_record.find('COMMAND_CLASS').text
-    sqltext = audit_record.find('SQLTEXT').text
-    # print(f"Timestamp: {timestamp}, Op: {op_name}, User: {user_info}, Host: {hostname}, IP: {IP_address}, Class: {command_class}, SQL: {sqltext}")
-
-    # Print syslog header
-    # Need to convert timestamp into RFC 5424
-    # TODO replace/convert timezone appropriately (need sample data to decide how)
-    # examples:
-    #    2019-01-18T11:07:53.520+07:00 
-    #    2019-01-18T11:07:53.520Z
-    print(f"<13>1 {timestamp} {hostname}",end=' ')
-    
-    # Print LEEF 2.0 header
-    print(f"2.0|BiSoft|mysqlaudit2leef|1.0|{record_id}|^|",end=' ')
-
-    # Print Event attributes
-    print(f"usrName={user_info}^sev=1^src={IP_address}^commClass={command_class}^sqlText={sqltext}")
--- mysqlaudit2leef.sh
+++ mysqlaudit2leef.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Script to convert MySQL Audit logs to LEEF format on the fly
 
+cat samplemysqlaudit.log | python3 mysqlaudit2leef.py
+
