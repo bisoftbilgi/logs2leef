5c27df9a1c1b47f3d1441e0ebe69cc333a1e95ed
Ozgur Yuksel <ozgur.yuksel@bisoft.com.tr>
2024-03-20 10:07:03 +0300

    Update shell script to handle local and remote target options

--- mysqlaudit2leef/mysqlaudit2leef.sh
+++ mysqlaudit2leef/mysqlaudit2leef.sh
@@ -2,21 +2,26 @@
 # Script to convert MySQL Audit logs to LEEF format on the fly
 #
 # Usage:
-#     $ mysqlaudit2leef.sh <MySQL Audit Logfile> <Remote syslog address> { start | stop }
+#     $ mysqlaudit2leef.sh <MySQL Audit Logfile> { <Remote syslog address> | local } { start | stop }
 #
 
 print_usage () {
-    echo "   USAGE: $0 <MySQL Audit Logfile> <Remote syslog address> { start | stop }"
+    echo "   USAGE: $0 <MySQL Audit Logfile> { <Remote syslog address> | local } { start | stop }"
     echo
 }
 
 start_run() {
-    # Invoke python3 script and feed into syslog
-    tail -f $AUDIT_LOGFILE | python3 mysqlaudit2leef.py | logger -n $SYSLOG_TARGET -t mysqlaudit2leef
+    if [ $SYSLOG_TARGET = 'local' ]; then
+        # Invoke python3 script and feed into remote syslog
+        tail -f $AUDIT_LOGFILE | python3 mysqlaudit2leef.py | logger -n $SYSLOG_TARGET -t mysqlaudit2leef
+    else
+        # Invoke python3 script and feed into local syslog
+        tail -f $AUDIT_LOGFILE | python3 mysqlaudit2leef.py | logger -t mysqlaudit2leef
+    fi
 }
 
 stop_run() {
-    LGRPID=$(ps -ef | grep "logger -n $SYSLOG_TARGET -t mysqlaudit2leef" | grep -v "grep" | awk '{print $2}')
+    LGRPID=$(ps -ef | grep "logger" | grep "-t mysqlaudit2leef" | grep -v "grep" | awk '{print $2}')
     CONVPID=$(ps -ef | grep "python3 mysqlaudit2leef.py" | grep -v "grep" | awk '{print $2}')
     TAILPID=$(ps -ef | grep "tail -f $AUDIT_LOGFILE" | grep -v "grep" | awk '{print $2}')
     kill $LGRPID
